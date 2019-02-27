47a748f85d1d700a126728a07905cd34de101658
teemak <bunth667@gmail.com>
2019-02-26 21:45:33 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const electron = require('electron');
+const {app, BrowserWindow, Menu, ipcMain } = electron;
+let mainWindow, addWindow;
+
+app.on('ready', () => { 
+	mainWindow = new BrowserWindow({});
+	mainWindow.loadURL(`file://${__dirname}/main.html`);
+	mainWindow.on('closed', () => app.quit());
+
+	const mainMenu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow() {
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Tee Money'
+	});
+	addWindow.loadURL(`file://${__dirname}/add.html`);
+	addWindow.on('closed', () => {
+		addWindow = null;
+	});
+}
+
+ipcMain.on('addTodo', (event, todo) => {
+	mainWindow.webContents.send('addTodo', todo);	
+	addWindow.close();
+});
+
+const menuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'New Todo',
+				accelerator: process.platform === 'darwin' ? 'command+w' : 'ctrl+w',
+				click() {
+					createAddWindow();
+				}
+			},
+			{ 
+				label: 'Quit',
+				accelerator: process.platform === 'darwin' ? 'Command+q' : 'Ctrl+q',
+				click() {
+					app.quit();
+				}
+			}
+
+		]
+	}
+];
+
+if(process.platform === 'darwin') {
+	menuTemplate.unshift();
+}
+
+if(process.env.NODE_ENV !== 'production') {
+	menuTemplate.push(
+		{
+			label: 'DEVELOPER',
+			submenu: [
+				{
+					role: 'reload'
+				},
+				{  
+					label: 'Toggle Developer Tools',
+					accelerator: process.platform === 'darwin' ? 'command+i' : 'ctrl+i',
+					click(item, focusedWindow) {
+						focusedWindow.toggleDevTools();	
+					}
+				}
+			]
+		}
+	);
+}
+
